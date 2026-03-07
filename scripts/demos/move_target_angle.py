a76949b81a686653139cb233f256453a5e2082c6
Chongjun Gao <156633562+gcj0418@users.noreply.github.com>
2026-03-07 15:52:19 +0800

    Create move_target_angle.py

---
+++ scripts/demos/move_target_angle.py
@@ -0,0 +1,59 @@
+import sys, math, time, numpy as np, rtde_control, rtde_receive
+from loguru import logger
+
+ROBOT_IP = "192.168.11.5"
+TARGET_POSE = [0.086, -0.374, 0.3725, 2.10, 0.39, -0.35]
+offset = [0, 0, -0.2, 0, 0, 0]         
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="{time} | {level} | {message}",
+    level="INFO",
+    enqueue=True,
+)
+
+try:
+    logger.info(f"Connecting to robot at {ROBOT_IP}")
+    rtde_c = rtde_control.RTDEControlInterface(ROBOT_IP)
+    rtde_r = rtde_receive.RTDEReceiveInterface(ROBOT_IP)
+
+    
+    START_POSE = rtde_c.poseTrans(TARGET_POSE, offset)
+    
+    logger.info("Computing inverse kinematics for start pose...")
+    start_pose_joints = rtde_c.getInverseKinematics(START_POSE, rtde_r.getActualQ())
+    print(start_pose_joints)
+    logger.info("IK computation successful for start pose.")
+    
+    logger.info("Computing inverse kinematics for target pose...")
+    target_pose_joints = rtde_c.getInverseKinematics(TARGET_POSE, rtde_r.getActualQ())
+    logger.info("IK computation successful for target pose.")
+    
+    logger.info("Moving to initial pose...")
+    rtde_c.moveJ(start_pose_joints, 0.5, 0.1)
+    time.sleep(1)  # 等待机器人稳定
+    
+    # # 移动到中间姿态
+    # logger.info("Moving to middle pose...")
+    # rtde_c.moveJ(middle_pose_joints, 0.5, 0.1)
+    # time.sleep(1)  # 等待机器人稳定
+    
+    # # 移动到目标姿态
+    logger.info("Moving to target pose...")
+    rtde_c.moveJ(target_pose_joints, 0.1, 0.1)
+    time.sleep(1)  # 等待机器人稳定
+    
+    # logger.info("Movement completed successfully!")
+    
+    # # 显示当前TCP位置
+    # current_pose = rtde_r.getActualTCPPose()
+    # logger.info(f"Current TCP pose: {[round(p, 4) for p in current_pose]}")
+    
+except Exception as e:
+    logger.error(f"An error occurred: {e}")
+    sys.exit(1)
+
+finally:
+    if 'rtde_c' in locals():
+        rtde_c.stopScript()
+        logger.info("Disconnected from robot.")
