5eefd25720689930e19b02d81f15fecc18385b57
Chongjun Gao <156633562+gcj0418@users.noreply.github.com>
2026-03-07 16:03:58 +0800

    Update move_target_angle.py

--- scripts/demos/move_target_angle.py
+++ scripts/demos/move_target_angle.py
@@ -2,6 +2,7 @@ import sys, math, time, numpy as np, rtde_control, rtde_receive
 from loguru import logger
 
 ROBOT_IP = "192.168.11.5"
+START_POSE = [0, -0.4, 0.3, math.pi, 0, 0]
 TARGET_POSE = [0.086, -0.374, 0.3725, 2.10, 0.39, -0.35]
 offset = [0, 0, -0.2, 0, 0, 0]         
 logger.remove()
@@ -16,21 +17,17 @@ try:
     logger.info(f"Connecting to robot at {ROBOT_IP}")
     rtde_c = rtde_control.RTDEControlInterface(ROBOT_IP)
     rtde_r = rtde_receive.RTDEReceiveInterface(ROBOT_IP)
-
     
-    START_POSE = rtde_c.poseTrans(TARGET_POSE, offset)
+    MIDDLE_POSE = rtde_c.poseTrans(TARGET_POSE, offset)
     
-    logger.info("Computing inverse kinematics for start pose...")
-    start_pose_joints = rtde_c.getInverseKinematics(START_POSE, rtde_r.getActualQ())
-    print(start_pose_joints)
-    logger.info("IK computation successful for start pose.")
+    start_pose_joint = rtde_c.getInverseKinematics(START_POSE, rtde_r.getActualQ())
+    middle_pose_joint = rtde_c.getInverseKinematics(MIDDLE_POSE, rtde_r.getActualQ())
     
-    logger.info("Moving to initial pose...")
-    rtde_c.moveJ(start_pose_joints, 0.5, 0.1)
-    time.sleep(1)  # 等待机器人稳定
+    rtde_c.moveJ(start_pose_joint, 0.1, 0.1)
+    time.sleep(1)
+    rtde_c.moveJ(middle_pose_joint, 0.1, 0.1)
+    time.sleep(1)
     
-    # # 移动到目标姿态
-    logger.info("Moving to target pose...")
     rtde_c.moveL(TARGET_POSE, 0.1, 0.1)
     time.sleep(1)
     
