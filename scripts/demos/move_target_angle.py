67b18c5a3b95fc2d427f47f73cebdcdb4ca30f56
Chongjun Gao <156633562+gcj0418@users.noreply.github.com>
2026-03-07 15:57:35 +0800

    Update move_target_angle.py

--- scripts/demos/move_target_angle.py
+++ scripts/demos/move_target_angle.py
@@ -25,29 +25,14 @@ try:
     print(start_pose_joints)
     logger.info("IK computation successful for start pose.")
     
-    logger.info("Computing inverse kinematics for target pose...")
-    target_pose_joints = rtde_c.getInverseKinematics(TARGET_POSE, rtde_r.getActualQ())
-    logger.info("IK computation successful for target pose.")
-    
     logger.info("Moving to initial pose...")
     rtde_c.moveJ(start_pose_joints, 0.5, 0.1)
     time.sleep(1)  # 等待机器人稳定
     
-    # # 移动到中间姿态
-    # logger.info("Moving to middle pose...")
-    # rtde_c.moveJ(middle_pose_joints, 0.5, 0.1)
-    # time.sleep(1)  # 等待机器人稳定
-    
     # # 移动到目标姿态
     logger.info("Moving to target pose...")
-    rtde_c.moveJ(target_pose_joints, 0.1, 0.1)
-    time.sleep(1)  # 等待机器人稳定
-    
-    # logger.info("Movement completed successfully!")
-    
-    # # 显示当前TCP位置
-    # current_pose = rtde_r.getActualTCPPose()
-    # logger.info(f"Current TCP pose: {[round(p, 4) for p in current_pose]}")
+    rtde_c.moveL(TARGET_POSE, 0.1, 0.1)
+    time.sleep(1)
     
 except Exception as e:
     logger.error(f"An error occurred: {e}")
